a3f94d3c030d6771c9dd6173c3a80f401c03f5a4
Nidhi <nrgautam7054@gmail.com>
2025-06-02 22:45:52 +0530

    data transformation is added

--- src/component/data_transformation.py
+++ src/component/data_transformation.py
@@ -28,13 +28,13 @@ class DataTransformation:
         
         '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
+            numerical_columns = ["writing score", "reading score"]
             categorical_columns = [
                 "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
+                "race/ethnicity",
+                "parental level of education",
                 "lunch",
-                "test_preparation_course",
+                "test preparation course",
             ]
 
             num_pipeline= Pipeline(
@@ -76,8 +76,8 @@ class DataTransformation:
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
+            train_dt=pd.read_csv(train_path)
+            test_dt=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
 
@@ -85,26 +85,26 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            target_column_name="math score"
+            numerical_columns = ["writing score", "reading score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            input_feature_train_dt=train_dt.drop(columns=[target_column_name],axis=1)
+            target_feature_train_dt=train_dt[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_dt=test_dt.drop(columns=[target_column_name],axis=1)
+            target_feature_test_dt=test_dt[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_dt)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_dt)
 
             train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
+                input_feature_train_arr, np.array(target_feature_train_dt)
             ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_dt)]
 
             logging.info(f"Saved preprocessing object.")
 
@@ -113,8 +113,7 @@ class DataTransformation:
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
 
-            )
-
+              )
             return (
                 train_arr,
                 test_arr,
